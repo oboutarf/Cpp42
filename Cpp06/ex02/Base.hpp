12bc2900b8cac8e5790a9eb047f7b007e93e1598
oscobouhou <osc.boutarfa@gmail.com>
2023-03-23 03:07:14 +0100

    { [finished CppModule06 ] }

--- Cpp06/ex02/Base.hpp
+++ Cpp06/ex02/Base.hpp
@@ -6,7 +6,7 @@
 /*   By: oboutarf <oboutarf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 19:48:25 by oboutarf          #+#    #+#             */
-/*   Updated: 2023/03/22 20:09:42 by oboutarf         ###   ########.fr       */
+/*   Updated: 2023/03/22 23:58:04 by oboutarf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,13 @@ class	C : public Base {};
 
 // Elle crée aléatoirement une instance de A, B ou C et la retourne en tant que pointeur sur
 // Base. Utilisez ce que vous souhaitez pour l’implémentation du choix aléatoire.
-Base *	generate(void);
+Base *	generate( void );
 
 // Elle affiche le véritable type de l’objet pointé par p : "A", "B" ou "C".
-void	identify(Base* p);
+void	identify( Base* p );
 
 // Elle affiche le véritable type de l’objet pointé par p : "A", "B" ou "C". Utiliser un pointeur
 // dans cette fonction est interdit.
-void	identify(Base& p);
+void	identify( Base& p );
 
 #endif
--- Cpp06/ex02/main.cpp
+++ Cpp06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oboutarf <oboutarf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 13:23:23 by oboutarf          #+#    #+#             */
-/*   Updated: 2023/03/22 20:16:30 by oboutarf         ###   ########.fr       */
+/*   Updated: 2023/03/23 03:05:40 by oboutarf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,69 @@ Base *	generate( void )	{
 	srand( time( 0 ) + randSeed );
 	randSeed++;
 	int	prob = rand() % 3;
-	std::cout << randSeed << std::endl;
 	if ( !prob )
-		return ( new A );
+		return ( std::cout << "baseType: generating derived class A from Base" << std::endl,  new A );
 	else if ( prob == 1 )
-		return ( new B ); 
-	return ( new C );
+		return ( std::cout << "baseType: generating derived class B from Base" << std::endl, new B ); 
+	return ( std::cout << "baseType: generating derived class C from Base" << std::endl, new C );
 }
 
+/* @ start transmission -- possible to catch exception with */
 void	identify( Base & p )	{
-	(void)p;
+	try {
+		A &a = dynamic_cast<A&>(p);
+		std::cout << "baseType: hello i'm base (a) and my adress is: " << &a << std::endl;
+		return ;
+	}
+	catch ( std::exception & e )	{
+	}
+	try {
+		B &b= dynamic_cast<B&>(p);
+		std::cout << "baseType: hello i'm base (b) and my adress is: " << &b << std::endl;
+		( void )b;
+		return ;
+	}
+	catch ( std::exception & e )	{
+	}
+	try {
+		C &c = dynamic_cast<C&>(p);
+		std::cout << "baseType: hello i'm base (c) and my adress is: " << &c << std::endl;
+		( void )c;
+		return ;
+	}
+	catch ( const std::exception& e )	{
+		std::cout << "baseType: Unknown type!" << std::endl;
+	}
 }
+
 
 void	identify( Base * p )	{
-	(void)p;
+	if ( A* a = dynamic_cast<A*>( p ) )	{
+		std::cout << "baseType: hello i'm base (a) " << std::endl;
+		return ;
+	}
+	if ( B* b = dynamic_cast<B*>( p ) )	{
+		std::cout << "baseType: hello i'm base (b) " << std::endl;
+		return ;
+	}
+	if ( C* c = dynamic_cast<C*>( p ) )	{
+		std::cout << "baseType: hello i'm base (c) " << std::endl;
+		return ;
+	}
+	std::cout << "baseType: unknown type, sorry." << std::endl;
+
 }
 
 Base::~Base( void )	{
-	std::cout << "base: a destructor was called" << std::endl;
+	std::cout << "baseType: a destructor was called" << std::endl;
 }
 
 
 int	main( void )	{
+	Base *baseCenter = generate();
+	identify( *baseCenter );
 
-	generate();
+	delete baseCenter;
+
 	return EXIT_SUCCESS ;
 }
