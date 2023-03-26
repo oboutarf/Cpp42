a78898426f1311431eab9621a8a708268ba546f9
oscobouhou <osc.boutarfa@gmail.com>
2023-03-26 17:59:46 +0200

    {}

--- Cpp08/ex02/MutantStack.hpp
+++ Cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: oboutarf <oboutarf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 13:28:38 by oboutarf          #+#    #+#             */
-/*   Updated: 2023/03/26 16:49:06 by oboutarf         ###   ########.fr       */
+/*   Updated: 2023/03/26 17:58:55 by oboutarf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,17 @@
 template <typename T> class MutantStack : public std::stack<T>	{
 
 public:
-	MutantStack() : std::stack<T>()						{}
-	MutantStack( MutantStack const & ref )				{ *this = ref; }
-	~MutantStack()										{}
-	MutantStack &	operator=( MutantStack const & rhs ){ (void) rhs; }
-	
-	typedef typename	std::stack<T>::container_type::iterator	iterator;
-	typedef typename	std::stack<T>::container_type::const_iterator	const_iterator;
+	MutantStack() : std::stack<T>()									{}
+	MutantStack &	operator=( const MutantStack & rhs )			{ std::stack<T>::operator=( rhs ); }
+	MutantStack( MutantStack const & ref ) : std::stack<T>( ref )	{}
+	virtual ~MutantStack()											{}
 
-	iterator	begin() 								{ return std::stack<T>::c.begin(); }
-	iterator	end() 									{ return std::stack<T>::c.end(); }
+	typedef typename	std::stack<T>::container_type::iterator			iterator;
+	typedef typename	std::stack<T>::container_type::const_iterator	const_iterator;
+	iterator		begin() 										{ return std::stack<T>::c.begin(); }
+	iterator		end() 											{ return std::stack<T>::c.end(); }
+	const_iterator	begin() const									{ return std::stack<T>::c.begin(); }
+	const_iterator	end() 	const									{ return std::stack<T>::c.end(); }
 };
 
 #endif
--- Cpp08/ex02/main.cpp
+++ Cpp08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oboutarf <oboutarf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 13:27:49 by oboutarf          #+#    #+#             */
-/*   Updated: 2023/03/26 16:47:52 by oboutarf         ###   ########.fr       */
+/*   Updated: 2023/03/26 17:58:29 by oboutarf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ int main()
 	mstack.push(5);
 	mstack.push(737);
 	mstack.push(0);
-
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
@@ -35,5 +34,11 @@ int main()
 		++it;
 	}
 	std::stack<int> s(mstack);
+	MutantStack<int>	stk(mstack);
+	MutantStack<int>::iterator	it1 = stk.begin();
+	std::cout << "STL Containers: new Mutant constructed by copy has size " << stk.size() << std::endl;
+	for (; it1 != stk.end(); it1++ )	{
+		std::cout << "Value is [" << *it1 << "]" << std::endl;
+	}
 	return 0;
 }
